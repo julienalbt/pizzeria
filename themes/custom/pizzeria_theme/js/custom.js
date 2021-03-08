fc8d81db0079c4fb65c4fec5376267eb70eab6f5
Julien <alombertjulien@gmail.com>
2021-03-08 17:17:42 +0100

    style

--- themes/custom/pizzeria_theme/js/custom.js
+++ themes/custom/pizzeria_theme/js/custom.js
@@ -13,6 +13,21 @@
       // Custom code here
       $('.is-active').parents('li').addClass('is-active');
 
+      $(document).on('click', 'a[href*=#]:not([href=#])', function($) {
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+          let target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length){
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 4000);
+            //return false;
+          }
+        }})
+
+
+
+
     }
   };
 
